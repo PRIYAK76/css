866d0b30d6972af0cb5114110ad22117fb168792
Your Name <you@example.com>
2023-08-11 13:07:18 +0530

    rf

---
+++ forimageoverlay/script.js
@@ -0,0 +1,15 @@
+let value=document.querySelectorAll(".num");
+let interval=4000;
+
+value.forEach((value)=>{
+    let start=0;
+    let end=parseInt(value.getAttribute("data-val"));
+    let du=math.floor(interval/end);
+    let coun=setInterval(function(){
+        start+=1;
+        value.textContent=start;
+        if(start==end){
+            clearInterval(coun);
+        }
+    },du);
+});
